88d0fb260451fe22904afb10b72ad4388537f2f3
Maria Martinez Bonilla <mmartinez@b7-23.local>
2017-12-06 16:55:20 -0600

    Adding sw and another funtionability

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,13 @@ var kss = require('kss')
 var eslint = require('gulp-eslint')
 var babel = require('gulp-babel')
 var concat = require('gulp-concat')
-
-var sassOptions = {
-  outputStyle: 'expanded',
-  eyeglass: {
-    enableImportOnce: false
-  }
-}
+var htmlmin = require('gulp-htmlmin')
+var uglify = require('gulp-uglify')
+var del = require('del')
+var imagemin = require('gulp-imagemin')
+var workbox = require('workbox-build');
+var runSequence = require('run-sequence');
+var dist = './dist';
 
 //
 // Begin Gulp Tasks.
@@ -32,18 +32,36 @@ var sassOptions = {
 // HTML Dev Workflow.
 //
 gulp.task('html:dev', function () {
-  return gulp.src(['src/**/*html', '!src/sass/*'])
+  return gulp.src(['src/**/*html', '!src/sass/**/*'])
     .pipe(gulp.dest('.tmp'))
     .pipe(connect.reload())
+})
+
+//
+// HTML Prod Workflow.
+//
+gulp.task('html:prod', function () {
+  return gulp.src(['src/**/*html', '!src/sass/**/*'])
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
 })
 
 //
 // Images Dev Workflow.
 //
 gulp.task('images:dev', function () {
-  return gulp.src('src/**/*.{png,jpg,jpeg,gif}')
+  return gulp.src('src/**/*.{png,jpg,jpeg,gif,svg}')
     .pipe(gulp.dest('.tmp'))
     .pipe(connect.reload())
+})
+
+//
+// Images Prod Workflow.
+//
+gulp.task('images:prod', function () {
+  return gulp.src('src/**/*.{png,jpg,jpeg,gif,svg}')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist'))
 })
 
 //
@@ -52,11 +70,31 @@ gulp.task('images:dev', function () {
 gulp.task('styles:dev', function () {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass(eyeglass(sassOptions)).on('error', sass.logError))
-    .pipe(prefix(["last 2 versions"]))
+    .pipe(sass(eyeglass({
+      outputStyle: 'expanded',
+      eyeglass: {
+        enableImportOnce: false
+      }
+    })).on('error', sass.logError))
+    .pipe(prefix(['last 2 versions']))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('.tmp/css'))
     .pipe(connect.reload())
+})
+
+//
+// CSS Prod Workflow.
+//
+gulp.task('styles:prod', function () {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass(eyeglass({
+      outputStyle: 'compressed',
+      eyeglass: {
+        enableImportOnce: false
+      }
+    })).on('error', sass.logError))
+    .pipe(prefix(['last 2 versions']))
+    .pipe(gulp.dest('dist/css'))
 })
 
 //
@@ -74,6 +112,27 @@ gulp.task('js:dev', function () {
 })
 
 //
+// Javascript Prod Workflow.
+//
+gulp.task('js:prod', function () {
+  return gulp.src('src/js/**/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+})
+
+//
+// Move manifest.
+//
+gulp.task('static', function () {
+  return gulp.src('static/*.*')
+    .pipe(gulp.dest('dist/static'))
+})
+
+//
 // Javascript linting.
 //
 gulp.task('lint', function () {
@@ -81,6 +140,23 @@ gulp.task('lint', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+})
+
+//
+// KSS Styleguide.
+//
+
+gulp.task('styleguide:generate', function () {
+  return kss({
+    source: 'src/sass',
+    destination: 'styleguide',
+    css: '../css/styles.css',
+    homepage: 'styleguide.md'
+  })
+})
+
+gulp.task('styleguide:watch', function () {
+  gulp.watch('src/sass/**/*', ['styleguide:generate'])
 })
 
 //
@@ -94,6 +170,23 @@ gulp.task('connect', function () {
 })
 
 //
+// Service worker.
+//
+gulp.task('generate-sw', () => {
+  return workbox.generateSW({
+    globDirectory: dist,
+    globPatterns: ['**\/*.{html,js,css,svg,jpg,png}'],
+    swDest: `${dist}/sw.js`,
+    clientsClaim: true,
+    skipWaiting: true
+  }).then(() => {
+    console.info('Service worker generation completed.');
+  }).catch((error) => {
+    console.warn('Service worker generation failed: ' + error);
+  });
+});
+
+//
 // Watch task.
 //
 gulp.task('watch', function () {
@@ -103,7 +196,20 @@ gulp.task('watch', function () {
   gulp.watch('src/js/**/*.js', ['lint', 'js:dev'])
 })
 
+gulp.task('clean', function () {
+  return del([
+    '.tmp',
+    'styleguide',
+    'dist'
+  ])
+})
+
 //
-// Task declarations.
+// Composite Task declarations.
 //
 gulp.task('dev', ['html:dev', 'images:dev', 'styles:dev', 'js:dev', 'connect', 'watch'])
+
+gulp.task('build', function () {
+  runSequence(['lint', 'html:prod', 'images:prod', 'styles:prod', 'js:prod', 'static'], 'generate-sw');
+})
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:watch'])
--- src/js/main.js
+++ src/js/main.js
@@ -1,86 +1,121 @@
-// ToDo: debe de replicar el array de imagenes, luego unificarlo y por ultimo desordenarlo
-var images = [{src: 'berengena.png', gameId: 1}, {src: 'mario.png', gameId: 2}, {src: 'luigui.png', gameId: 3}, {src: 'zanahoria.png', gameId: 4}, {src: 'honguito.png', gameId: 5}, {src: 'cherry.png', gameId: 6}, {src: 'pacman.png', gameId: 7}, {src: 'pacwoman.png', gameId: 8}, {src: 'ghost1.png', gameId: 9}, {src: 'ghost2.png', gameId: 10}]
-var bgImages = images.concat(images)
-var cardContainer = document.getElementById('cardContainer')
-var cards = cardContainer.children
-var yourScore = document.getElementById('yourScore')
-var score = 0
-var cardsLog = []
-var clikedCounter = 0
-var clickedCards = []
+const images = [{ src: 'berengena.png', gameId: 1 }, { src: 'mario.png', gameId: 2 }, { src: 'luigui.png', gameId: 3 }, { src: 'zanahoria.png', gameId: 4 }, { src: 'honguito.png', gameId: 5 }, { src: 'cherry.png', gameId: 6 }, { src: 'pacman.png', gameId: 7 }, { src: 'pacwoman.png', gameId: 8 }, { src: 'ghost1.png', gameId: 9 }, { src: 'ghost2.png', gameId: 10 }];
+const bgImages = images.concat(images);
+const cardContainer = document.getElementById('cardContainer');
+const cards = cardContainer.children;
+const yourScore = document.getElementById('yourScore');
+let score = 0;
+const cardsLog = [];
+let clikedCounter = 0;
+let clickedCards = [];
+let stopClick = false;
+let pushToLog = false;
+let success = false;
+let successPair = 0;
 
-var randomObj = bgImages.sort(function () {
-  return 0.5 - Math.random()
-})
+const randomObj = bgImages.sort(() => {
+  return 0.5 - Math.random();
+});
 
-// ToDo: Hacer que la carta despues de un tiempo se vuelva.
-var turnCard = function turnCard (card) {
-  setTimeout(function () {
-    card.style.backgroundImage = 'none'
-    card.classList.remove('front')
-  }, 2000)
-}
+const turnCard = function turnCard(card) {
+  setTimeout(() => {
+    card.style.backgroundImage = 'none';
+    card.classList.remove('front');
+    stopClick = false;
+    clikedCounter = 0;
+  }, 1500);
+};
 
-// ToDo: al iniciar debe de  acomodar las imagenes random por cada carta y mostrarlas por 5s
-var setImage = function setImage (card, index) {
-  card.classList.add('front')
-  card.style.backgroundImage = 'url("../img/' + randomObj[index].src
-}
+const setImage = function setImage(card, index) {
+  card.classList.add('front');
+  card.style.backgroundImage = `url("../img/${randomObj[index].src}`;
+};
 
-// ToDo: cuando las cartas coincidan ganar 100 pts, cuando la carta haya sido vista y no coincida perder 50 pts
-// ToDo: cuando 2 cartas coincidan deben quedar visibles
-var verifingCard = function verifingCard (card, cardId) {
+const verifingCard = function verifingCard() {
   if (clickedCards[1] !== undefined && clickedCards[0].gameId === clickedCards[1].gameId) {
-    score = score + 100
-    yourScore.innerHTML = score
-    clickedCards.forEach(obj => {
-      obj.node.classList.add('hidden')
-    })
-    clickedCards = []
-    clikedCounter = -1
-  } else if (clikedCounter === 1) {
-    clikedCounter = -1
+    success = true;
+    score += 100;
+    yourScore.innerHTML = score;
+    clickedCards.forEach((obj) => {
+      obj.node.classList.add('hidden');
+    });
+    clickedCards = [];
+    clikedCounter = 0;
+    successPair += 1;
+  } else if (clikedCounter === 2) {
+    success = false;
+    clickedCards.forEach((obj) => {
+      turnCard(obj.node);
+    });
+    clickedCards = [];
+  }
+};
+
+const verifiedCardLog = function verifiedCardLog(cardId) {
+  if (cardsLog.length) {
     cardsLog.forEach((card) => {
-      if (card === cardId) {
-        score = score - 50
-        yourScore.innerHTML = score
-        // borre la ultima que clickeo
+      if (card === cardId && !success) {
+        score -= 50;
+        yourScore.innerHTML = score;
+        const arrIdx = cardsLog.indexOf(card);
+        cardsLog.splice(arrIdx, 1);
+      } else {
+        pushToLog = true;
       }
-    })
-    clickedCards.forEach(obj => {
-      turnCard(obj.node)
-    })
-    clickedCards = []
-  }
-}
-
-// ToDo: cuando le de click a la carta debe de cambiar el display de la carta
-// ToDo: Arreglar clicker counter
-var clikedCard = function clikedCard (card, index) {
-  card.addEventListener('click', function () {
-    clickedCards.push({'node': card, 'gameId': randomObj[index].gameId})
-    if (clickedCards[1] !== undefined && clickedCards[0].node.id === clickedCards[1].node.id) {
-      alert('Clickee otra carta papita')
-      turnCard(clickedCards[1].node)
-      clickedCards = []
-      return true
+    });
+    if (pushToLog) {
+      cardsLog.push(cardId);
+      pushToLog = false;
     }
-    setImage(card, index)
-    verifingCard(card, card.id)
-    clikedCounter = clikedCounter + 1 // Trabajarlo
-    cardsLog.push(card.id)
-  })
-}
-
-var init = function init () {
-  for (let index = 0; index < cards.length; index++) {
-    let cardsId = cards[index].id
-    let card = document.getElementById(cardsId)
-    setImage(card, index)
-    turnCard(card)
-    clikedCard(card, index)
+  } else {
+    cardsLog.push(cardId);
   }
-}
-init()
+};
 
-// ToDo: que sea posible solo clickear 2 cartas a la vez.
+const clikedCard = function clikedCard(card, index) {
+  card.addEventListener('click', () => {
+    if (stopClick) {
+      return false;
+    }
+    clikedCounter += 1;
+    clickedCards.push({ node: card, gameId: randomObj[index].gameId });
+    if (clickedCards[1] !== undefined && clickedCards[0].node.id === clickedCards[1].node.id) {
+      alert('Por favor haga click sobre otra carta');
+      turnCard(clickedCards[1].node);
+      clickedCards = [];
+      return true;
+    }
+    setImage(card, index);
+    verifingCard();
+    verifiedCardLog(card.id);
+    if (clikedCounter >= 2) {
+      stopClick = true;
+    }
+    if (successPair === 10) {
+      alert('Usted ha ganado por favor haga click en REINICIAR');
+    }
+    return true;
+  });
+};
+
+
+
+const init = function init() {
+  for (let index = 0; index < cards.length; index += 1) {
+    const cardsId = cards[index].id;
+    const card = document.getElementById(cardsId);
+    setImage(card, index);
+    turnCard(card);
+    clikedCard(card, index);
+  }
+};
+init();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
